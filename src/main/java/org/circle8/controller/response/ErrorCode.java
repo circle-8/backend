83c16f3edfdf31392a6b5e0c9ce673de88be7701
German Merkel <germanmerkel9@gmail.com>
2023-07-01 15:15:36 -0300

    Error responses (#5)

--- src/main/java/org/circle8/controller/response/ErrorCode.java
+++ src/main/java/org/circle8/controller/response/ErrorCode.java
@@ -5,13 +5,18 @@ import io.javalin.http.HttpStatus;
 public enum ErrorCode {
 	INTERNAL_ERROR,
 	BAD_REQUEST,
-	NOT_FOUND;
+	NOT_FOUND,
+	TOKEN_ERROR,
+	TOKEN_NOT_FOUND,
+	TOKEN_EXPIRED
+	;
 
 	public HttpStatus toStatus() {
 		return switch(this) {
 			case INTERNAL_ERROR -> HttpStatus.INTERNAL_SERVER_ERROR;
 			case BAD_REQUEST -> HttpStatus.BAD_REQUEST;
 			case NOT_FOUND -> HttpStatus.NOT_FOUND;
+			case TOKEN_ERROR, TOKEN_NOT_FOUND, TOKEN_EXPIRED -> HttpStatus.UNAUTHORIZED;
 		};
 	}
 }
