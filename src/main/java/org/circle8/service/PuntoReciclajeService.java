f13d81734ea712afcf14f01cf29952409e8fe02c
Michael Giordano <70980802+maikgiordano98@users.noreply.github.com>
2023-07-19 21:21:47 -0300

    Update src/main/java/org/circle8/service/PuntoReciclajeService.java
    Co-authored-by: German Merkel <germanmerkel9@gmail.com>

--- src/main/java/org/circle8/service/PuntoReciclajeService.java
+++ src/main/java/org/circle8/service/PuntoReciclajeService.java
@@ -37,7 +37,7 @@ public class PuntoReciclajeService {
 	/**
 	 * Obtiene un punto de reciclaje por medio de su id
 	 */
-	public PuntoReciclajeDto get(Long id, Long recicladorId) throws ServiceError, NotFoundException {
+	public PuntoReciclajeDto get(Long id, Long recicladorId) throws ServiceException {
 		try {
 			return PuntoReciclajeDto.from(this.dao.get(id, recicladorId).
 				orElseThrow(() -> new NotFoundException("No existe el punto de reciclaje")));
