2e5d576d134fad53ffb1cd34eed0dcdd776e1db9
Michael Giordano <70980802+maikgiordano98@users.noreply.github.com>
2023-07-21 18:26:14 -0300

    add get punto de reciclaje (#16)
    * add get punto de reciclaje
    
    * refactor accord comments
    
    * modified accord comments
    
    * modified accord comments
    
    * hacha
    
    * Update src/main/java/org/circle8/service/PuntoReciclajeService.java
    
    Co-authored-by: German Merkel <germanmerkel9@gmail.com>
    
    * Update src/main/java/org/circle8/service/PuntoReciclajeService.java
    
    Co-authored-by: German Merkel <germanmerkel9@gmail.com>
    
    * Update src/main/java/org/circle8/dao/PuntoReciclajeDao.java
    
    Co-authored-by: German Merkel <germanmerkel9@gmail.com>
    
    * add tests
    
    * add throw
    
    ---------
    
    Co-authored-by: German Merkel <germanmerkel9@gmail.com>

--- src/main/java/org/circle8/dao/PuntoReciclajeDao.java
+++ src/main/java/org/circle8/dao/PuntoReciclajeDao.java
@@ -3,10 +3,7 @@ package org.circle8.dao;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 
 import javax.sql.DataSource;
@@ -21,7 +18,7 @@ import org.circle8.filter.PuntoReciclajeFilter;
 
 import com.google.inject.Inject;
 
-public class PuntoReciclajeDao extends Dao{
+public class PuntoReciclajeDao extends Dao {
 
 	private static final String WHERE_AREA = """
 		AND pr."Latitud" BETWEEN ? AND ?
@@ -37,13 +34,23 @@ public class PuntoReciclajeDao extends Dao{
 		    WHERE 1=1
 		""";
 
+	private static final String SELECT_GET = """
+		SELECT pr."ID", pr."Titulo", pr."Latitud", pr."Longitud", pr."DiasAbierto", pr."CiudadanoId" , prtr."TipoResiduoId", tr."Nombre", ciu."UsuarioId"
+		  FROM "PuntoReciclaje" AS pr
+		  LEFT JOIN "PuntoReciclaje_TipoResiduo" AS prtr ON prtr."PuntoReciclajeId" = pr."ID"
+		  LEFT JOIN "TipoResiduo" AS tr on tr."ID" = prtr."TipoResiduoId"
+		  LEFT JOIN "Ciudadano" AS ciu on pr."CiudadanoId" = ciu."ID"
+		 WHERE pr."ID" = ?
+		   AND pr."CiudadanoId" = ?
+		""";
+
 	private static final String WHERE_CIUDADADO_NULL = """
-			AND pr."CiudadanoId" IS NULL
-			""";
+		AND pr."CiudadanoId" IS NULL
+		""";
 
 	private static final String WHERE_CIUDADADO_NOT_NULL = """
-			AND pr."CiudadanoId" IS NOT NULL
-			""";
+		AND pr."CiudadanoId" IS NOT NULL
+		""";
 
 	private static final String WHERE_TIPO = """
 		    AND pr."ID" IN (
@@ -63,12 +70,24 @@ public class PuntoReciclajeDao extends Dao{
 	/**
 	 * Obtiene el listado de puntos de reciclaje
 	 */
-	public List<PuntoReciclaje> list(PuntoReciclajeFilter filter) throws PersistenceException{
-		try ( var t = open(true); var select = createSelectForList(t, filter) ) {
-			try ( var rs = select.executeQuery() ) {
+	public List<PuntoReciclaje> list(PuntoReciclajeFilter filter) throws PersistenceException {
+		try (var t = open(true); var select = createSelectForList(t, filter)) {
+			try (var rs = select.executeQuery()) {
 				return getList(rs, filter);
 			}
-		} catch ( SQLException e ) {
+		} catch (SQLException e) {
+			throw new PersistenceException("error getting punto reciclaje", e);
+		}
+	}
+
+	/**
+	 * Obtiene un punto de reciclaje por medio de su id
+	 */
+	public Optional<PuntoReciclaje> get(Long id, Long recicladorId) throws PersistenceException {
+		try (var t = open(true); var select = createSelectForGet(t, id, recicladorId);
+			 var rs = select.executeQuery()) {
+			return Optional.ofNullable(getPunto(rs));
+		} catch (SQLException e) {
 			throw new PersistenceException("error getting punto reciclaje", e);
 		}
 	}
@@ -78,12 +97,12 @@ public class PuntoReciclajeDao extends Dao{
 	 * Agrupa los tipos de residuo por cada punto
 	 * Valida el filtro de dias
 	 */
-	private List<PuntoReciclaje> getList(ResultSet rs, PuntoReciclajeFilter filter) throws SQLException{
+	private List<PuntoReciclaje> getList(ResultSet rs, PuntoReciclajeFilter filter) throws SQLException {
 		var mapPuntos = new HashMap<Long, PuntoReciclaje>();
-		while ( rs.next() ) {
+		while (rs.next()) {
 			val id = rs.getLong("ID");
 			PuntoReciclaje punto = mapPuntos.get(id);
-			if ( punto == null) {
+			if (punto == null) {
 				punto = new PuntoReciclaje(
 					id,
 					rs.getString("Titulo"),
@@ -96,7 +115,7 @@ public class PuntoReciclajeDao extends Dao{
 				);
 				mapPuntos.put(id, punto);
 			}
-			if(rs.getInt("TipoResiduoId") != 0)
+			if (rs.getInt("TipoResiduoId") != 0)
 				punto.tipoResiduo.add(new TipoResiduo(rs.getInt("TipoResiduoId"), rs.getString("Nombre")));
 		}
 
@@ -106,6 +125,29 @@ public class PuntoReciclajeDao extends Dao{
 	}
 
 	/**
+	 * Procesa el resultado de la consulta de get
+	 */
+	private PuntoReciclaje getPunto(ResultSet rs) throws SQLException {
+		PuntoReciclaje punto = null;
+		if (rs.next()) {
+			punto = new PuntoReciclaje(
+				rs.getLong("ID"),
+				rs.getString("Titulo"),
+				rs.getDouble("Latitud"),
+				rs.getDouble("Longitud"),
+				Dia.getDia(rs.getString("DiasAbierto")),
+				new ArrayList<>(),
+				rs.getLong("CiudadanoId"),
+				User.builder().id(rs.getLong("UsuarioId")).build()
+			);
+			if (rs.getInt("TipoResiduoId") != 0)
+				punto.tipoResiduo.add(new TipoResiduo(rs.getInt("TipoResiduoId"), rs.getString("Nombre")));
+		}
+		return punto;
+	}
+
+	/**
+	 *
 	 */
 	private PreparedStatement createSelectForList(Transaction t, PuntoReciclajeFilter f) throws PersistenceException, SQLException {
 		var b = new StringBuilder(SELECT);
@@ -113,21 +155,21 @@ public class PuntoReciclajeDao extends Dao{
 
 		b.append(f.isPuntoVerde() ? WHERE_CIUDADADO_NULL : WHERE_CIUDADADO_NOT_NULL);
 
-		if ( f.hasTipo() ) {
+		if (f.hasTipo()) {
 			String marks = f.tiposResiduos.stream()
-					.map(tr -> "?")
-					.collect(Collectors.joining(","));
+				.map(tr -> "?")
+				.collect(Collectors.joining(","));
 
 			b.append(String.format(WHERE_TIPO, marks));
 			parameters.addAll(f.tiposResiduos);
 		}
 
-		if( f.hasReciclador() ) {
+		if (f.hasReciclador()) {
 			b.append("AND pr.\"CiudadanoId\" = ?\n");
 			parameters.add(f.reciclador_id);
 		}
 
-		if ( f.hasArea() ) {
+		if (f.hasArea()) {
 			b.append(WHERE_AREA);
 			parameters.add(f.latitud - f.radio);
 			parameters.add(f.latitud + f.radio);
@@ -137,7 +179,17 @@ public class PuntoReciclajeDao extends Dao{
 
 		var p = t.prepareStatement(b.toString());
 		for (int i = 0; i < parameters.size(); i++)
-			p.setObject(i+1, parameters.get(i));
+			p.setObject(i + 1, parameters.get(i));
+
+		return p;
+	}
+
+	private PreparedStatement createSelectForGet(Transaction t, Long id, Long recicladorId) throws PersistenceException, SQLException {
+		var b = SELECT_GET;
+
+		var p = t.prepareStatement(b.toString());
+		p.setLong(1, id);
+		p.setLong(2, recicladorId);
 
 		return p;
 	}
--- src/main/java/org/circle8/service/PuntoReciclajeService.java
+++ src/main/java/org/circle8/service/PuntoReciclajeService.java
@@ -4,8 +4,11 @@ import java.util.List;
 
 import org.circle8.dao.PuntoReciclajeDao;
 import org.circle8.dto.PuntoReciclajeDto;
+import org.circle8.entity.PuntoReciclaje;
+import org.circle8.exception.NotFoundException;
 import org.circle8.exception.PersistenceException;
 import org.circle8.exception.ServiceError;
+import org.circle8.exception.ServiceException;
 import org.circle8.filter.PuntoReciclajeFilter;
 
 import com.google.inject.Inject;
@@ -27,6 +30,19 @@ public class PuntoReciclajeService {
 	public List<PuntoReciclajeDto> list(PuntoReciclajeFilter filter) throws ServiceError{
 		try {
 			return this.dao.list(filter).stream().map(PuntoReciclajeDto::from).toList();
+		} catch (PersistenceException e) {
+			throw new ServiceError("Ha ocurrido un error al obtener el listado de puntos de reciclaje", e);
+		}
+	}
+
+	/**
+	 * Obtiene un punto de reciclaje por medio de su id
+	 */
+	public PuntoReciclajeDto get(Long id, Long recicladorId) throws ServiceException {
+		try {
+			return this.dao.get(id, recicladorId)
+				.map(PuntoReciclajeDto::from)
+				.orElseThrow(() -> new NotFoundException("No existe el punto de reciclaje"));
 		} catch (PersistenceException e) {
 			throw new ServiceError("Ha ocurrido un error al obtener el listado de puntos de reciclaje", e);
 		}
