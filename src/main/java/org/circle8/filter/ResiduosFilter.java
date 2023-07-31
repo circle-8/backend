792296592bacb370420ae161662593243e8ed6e8
NewtonGauss <germanmerkel9@gmail.com>
2023-07-30 23:08:37 -0300

    GET `/residuos`

---
+++ src/main/java/org/circle8/filter/ResiduosFilter.java
@@ -0,0 +1,29 @@
+package org.circle8.filter;
+
+import lombok.ToString;
+import org.circle8.utils.Parser;
+
+import java.time.ZonedDateTime;
+import java.util.List;
+import java.util.Map;
+
+@ToString
+public class ResiduosFilter {
+	public List<Long> puntosResiduo;
+	public List<Long> ciudadanos;
+	public List<Long> tipos;
+	public Long transaccion;
+	public Long recorrido;
+	public Boolean retirado;
+	public ZonedDateTime fechaLimiteRetiro;
+
+	public ResiduosFilter(Map<String, List<String>> queryParams) {
+		this.puntosResiduo = Parser.parseLongList(queryParams.getOrDefault("puntos_residuo", List.of()));
+		this.ciudadanos = Parser.parseLongList(queryParams.getOrDefault("ciudadanos", List.of()));
+		this.tipos = Parser.parseLongList(queryParams.getOrDefault("tipos", List.of()));
+		this.transaccion = Parser.parseLong(queryParams, "transaccion");
+		this.recorrido = Parser.parseLong(queryParams, "recorrido");
+		this.retirado = queryParams.containsKey("retirado") ? Boolean.valueOf(queryParams.get("retirado").get(0)) : null;
+		this.fechaLimiteRetiro = Parser.parseLocalZonedDateTime(queryParams, "fecha_limite_retiro");
+	}
+}
--- src/main/java/org/circle8/utils/Parser.java
+++ src/main/java/org/circle8/utils/Parser.java
@@ -11,25 +11,25 @@ import org.circle8.controller.request.IRequest.Validation;
 import jakarta.annotation.Nullable;
 
 public class Parser {
-	
 	@Nullable
-	public static String parseString(Validation validation,Map<String, List<String>> queryParams, String paramName) {
-		return queryParams.getOrDefault(paramName, List.of("")).get(0);
-	}
-	
-	@Nullable
-	public static Long parseLong(Validation validation,Map<String, List<String>> queryParams, String paramName) {
+	public static Long parseLong(Validation validation, Map<String, List<String>> queryParams, String paramName) {
 		try {
 			var param = queryParams.getOrDefault(paramName, List.of());
 			return !param.isEmpty() ? Long.parseLong(param.get(0)) : null;
 		} catch ( NumberFormatException e ) {
-			validation.add(String.format("%s debe ser un numero", paramName));
+			if ( validation != null )
+				validation.add(String.format("%s debe ser un numero", paramName));
 			return null;
 		}
 	}
 
 	@Nullable
-	public static Double parseDouble(Validation validation,Map<String, List<String>> queryParams, String paramName) {
+	public static Long parseLong(Map<String, List<String>> queryParams, String paramName) {
+		return parseLong(null, queryParams, paramName);
+	}
+
+	@Nullable
+	public static Double parseDouble(Validation validation, Map<String, List<String>> queryParams, String paramName) {
 		try {
 			var param = queryParams.getOrDefault(paramName, List.of());
 			return !param.isEmpty() ? Double.parseDouble(param.get(0)) : null;
@@ -38,15 +38,29 @@ public class Parser {
 			return null;
 		}
 	}
-	
+
 	@Nullable
-	public static ZonedDateTime parseLocalZonedDateTime(Validation validation,Map<String, List<String>> queryParams, String paramName) {
+	public static ZonedDateTime parseLocalZonedDateTime(Validation validation, Map<String, List<String>> queryParams, String paramName) {
 		try {
 			var param = queryParams.getOrDefault(paramName, List.of());
 			return !param.isEmpty() ? ZonedDateTime.parse(param.get(0),DateTimeFormatter.ISO_OFFSET_DATE_TIME) : null;
 		} catch ( DateTimeParseException e ) {
-			validation.add(String.format("%s debe ser en formato ISO_OFFSET_DATE_TIME (2023-07-19T23:13:14.445Z)", paramName));
+			if ( validation != null )
+				validation.add(String.format("%s debe ser en formato ISO_OFFSET_DATE_TIME (2023-07-19T23:13:14.445Z)", paramName));
 			return null;
-		}		
+		}
+	}
+
+	@Nullable
+	public static ZonedDateTime parseLocalZonedDateTime(Map<String, List<String>> queryParams, String paramName) {
+		return parseLocalZonedDateTime(null, queryParams, paramName);
+	}
+
+	public static List<Long> parseLongList(List<String> l) {
+		try {
+			return l.stream().map(Long::parseLong).toList();
+		} catch ( NumberFormatException e ) {
+			return List.of();
+		}
 	}
 }
