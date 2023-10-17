342093d23c4603474db7b14f1773fa087a8dcbd3
germanalopez <38166616+germanalopez@users.noreply.github.com>
2023-10-17 20:30:04 -0300

    add validacion + test (#152)

--- src/main/java/org/circle8/service/SolicitudService.java
+++ src/main/java/org/circle8/service/SolicitudService.java
@@ -53,11 +53,18 @@ public class SolicitudService {
 
 			if ( residuo.fechaRetiro != null )
 				throw new ServiceException("El residuo ya se ha retirado");
+			
+			if( residuo.transaccion != null 
+					&& residuo.transaccion.id != 0)
+				throw new ServiceException("El residuo ya es parte de una transacción");
+			
+			if( residuo.recorrido != null 
+					&& residuo.recorrido.id != 0)
+				throw new ServiceException("El residuo ya es parte de un circuito de reciclaje");
 
 			val punto = puntoDao.get(t, puntoReciclajeId)
 				.orElseThrow(() -> new NotFoundException("No existe el punto de reciclaje"));
 
-			/* Validar que el punto de reciclaje acepte el tipo de residuo */
 			val acceptsTipo = punto.tipoResiduo.stream().anyMatch(tr -> tr.id == residuo.tipoResiduo.id);
 			if ( !acceptsTipo )
 				throw new ServiceException("El Punto de Reciclaje no admite este tipo de residuo");
